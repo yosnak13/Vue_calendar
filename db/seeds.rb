c9399ff68ac0887b9f874529325bcec9098a9a90
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-08-12 17:26:22 +0900

    rails db:seed new

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ Event.create!(name: '予定6', start: '2021-7-27 10:00:00', end: '2021-7-27 11:0
 Event.create!(name: '予定7', start: '2021-7-27 10:00:00', end: '2021-7-27 11:00:00')
 Event.create!(name: '予定8', start: '2021-7-27 10:00:00', end: '2021-7-27 11:00:00')
 Event.create!(name: '予定9', start: '2021-7-27 10:00:00', end: '2021-7-27 11:00:00')
+
+Calendar.create!(name: '予定')
